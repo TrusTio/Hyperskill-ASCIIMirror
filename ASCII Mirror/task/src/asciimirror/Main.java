25eec262f4903bdedd17ebe370e667e4a85ed2e5
TrusTio <erkan.myumyunov@gmail.com>
2025-03-30 16:19:02 +0300

    Stage 4/5 completed

--- ASCII Mirror/task/src/asciimirror/Main.java
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -2,11 +2,13 @@ package asciimirror;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 /**
- * Stage 3/5 completed
+ * Stage 4/5 completed
  * ASCII Mirror with Java - https://hyperskill.org/projects/260
  * Part of Hyperskill's Java Backend Developer (Spring Boot) course.
  */
@@ -19,6 +21,7 @@ public class Main {
         String filePath = scanner.nextLine();
         File file = new File(filePath);
 
+        List<String> lineList = new ArrayList<>();
 
         logger.info("\u001B[33m" + "File information: " +
                     "\n File name: " + file.getName() +
@@ -28,14 +31,41 @@ public class Main {
                     "\n Exists: " + file.exists() +
                     "\n Parent path: " + file.getParent() +
                     "\n Absolute path: " + file.getAbsolutePath());
+
+
+        int longestStringLength = 0;
         try (Scanner fileScanner = new Scanner(file)) {
             while (fileScanner.hasNext()) {
-
-                System.out.println(fileScanner.nextLine());
-
+                String nextLine = fileScanner.nextLine();
+                lineList.add(nextLine);
+                if(nextLine.length() > longestStringLength) {
+                    longestStringLength = nextLine.length();
+                }
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         }
+
+
+        List<String> mirroredList = mirrorList(lineList,longestStringLength);
+        for(String str : mirroredList){
+            System.out.println(str);
+        }
+    }
+
+    /**
+     * Mirrors the list given a List of String and an int of the longest string length
+     * @param lineList
+     * @param longestStringLength
+     * @return
+     */
+    public static List<String> mirrorList (List<String> lineList, int longestStringLength){
+        List<String> mirroredList = new ArrayList<>() ;
+
+        for(String str : lineList) {
+                int lengthDifference = longestStringLength - str.length();
+                mirroredList.add(str + " ".repeat(lengthDifference) + " | " + str + " ".repeat(lengthDifference));
+        }
+        return mirroredList;
     }
 }
